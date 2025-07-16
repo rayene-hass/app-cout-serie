5ee198a73d1946a25c8ad33370daf4715a7802da
rayene-hass <rayene.hassan@avatarmobilite.com>
2025-07-16 12:07:01 +0200

    Update streamlit_app.py
    v6

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ def get_comp_key(row):
 
 # Titre principal de l'application
 st.title("Estimation du coût de revient d’un véhicule en fonction de la quantité")
-st.markdown("Version: v5")
+st.markdown("Version: v6")
 
 # 1. Chargement de la nomenclature depuis Google Sheets
 
@@ -59,8 +59,6 @@ def charger_nomenclature_gsheet():
     worksheet = sheet.worksheet(st.secrets["sheets"]["worksheet_nomenclature"])
     data = worksheet.get_all_records()
     df = pd.DataFrame(data)
-    st.subheader("Debug données Google Sheets")
-    st.write(df.head(10))
 
 
     # Nettoyage et normalisation
@@ -82,6 +80,17 @@ def charger_nomenclature_gsheet():
     if "Coût moule (€)" not in df.columns:
         df["Coût moule (€)"] = None
 
+     colonnes_numeriques = ["Masse (kg)", "Prix matière (€/kg)","Coût moule (€)", "Prix Effectif / Véhicule", "Quantité / Véhicule"]
+    for col in colonnes_numeriques:
+        if col in df.columns:
+            df[col] = (
+                df[col]
+                .astype(str)
+                .str.replace(r"[^\d,.\-]", "", regex=True)  # Supprime € / espaces etc.
+                .str.replace(",", ".", regex=False)         # Transforme , en .
+            )
+            df[col] = pd.to_numeric(df[col], errors="coerce")
+    
     return df
 
 @st.cache_data(ttl=10)
